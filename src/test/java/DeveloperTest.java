3e0271578a459047e7c85d4c95ba37829e329d7d
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-02-03 16:35:44 +0000

    Adds tests for Developer class

--- src/test/java/DeveloperTest.java
+++ src/test/java/DeveloperTest.java
@@ -1,2 +1,42 @@
+import org.junit.Before;
+import org.junit.Test;
+import techStaff.Developer;
+
+import static org.junit.Assert.assertEquals;
+
 public class DeveloperTest {
+
+    private Developer developer;
+
+    @Before
+    public void before(){
+        developer = new Developer("Henry MacDonald", "DE264364P", 25000.0);
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Henry MacDonald", developer.getName());
+    }
+
+    @Test
+    public void hasNINumber(){
+        assertEquals("DE264364P", developer.getNINumber());
+    }
+
+    @Test
+    public void hasSalary(){
+        assertEquals(25000.0, developer.getSalary(), 0.01);
+    }
+
+    @Test
+    public void canRaiseSalary(){
+        developer.raiseSalary(3.0);
+        assertEquals(25750.0, developer.raiseSalary(), 0.01);
+    }
+
+    @Test
+    public void canPayBonus(){
+        developer.payBonus();
+        assertEquals(250.0, developer.payBonus(), 0.01);
+    }
 }
