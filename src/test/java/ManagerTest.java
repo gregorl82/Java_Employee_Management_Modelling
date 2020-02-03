95a95f5cc2961f919ab26b6926462762c36ed139
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-02-03 15:18:00 +0000

    Adds tests for Manager class

--- src/test/java/ManagerTest.java
+++ src/test/java/ManagerTest.java
@@ -1,2 +1,49 @@
+import management.Manager;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ManagerTest {
+
+    private Manager manager;
+
+    @Before
+    public void before(){
+        manager = new Manager("Jimbob Walton", "XV023407P", 45000.0, "Cattle Herding");
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Jimbob Walton", manager.getName());
+    }
+
+    @Test
+    public void hasNINumber(){
+        assertEquals("XV023407P", manager.getNINumber());
+    }
+
+    @Test
+    public void hasSalary(){
+        assertEquals(45000.0, manager.getSalary());
+    }
+
+    @Test
+    public void hasADepartmentName(){
+        assertEquals("Cattle Herding", manager.getDepartment());
+    }
+
+    @Test
+    public void canRaiseSalary(){
+        manager.raiseSalary(4.5);
+        assertEquals(47025.0, manager.getSalary());
+    }
+
+    @Test
+    public void canPayBonus(){
+        assertEquals(450, manager.payBonus());
+    }
+
+
+
 }
